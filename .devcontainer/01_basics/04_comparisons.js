c35f8b634db41052661875b834fa590e8168108b
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-09 21:05:10 +0000

    comparisons

---
+++ .devcontainer/01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+// console.log("2">1); // bcoz it converts "2" in int
+
+// console.log(null>0); // false
+// console.log(null==0); // false
+// console.log(null>=0); // false // comparator change null to 0 but  == works differently
+
+// console.log(undefined>0); // false
+// console.log(undefined==0); // false
+// console.log(undefined>=0); // false
+
+// ==== // strict check // it checks value as well as datatypes
+
+console.log("2" === 2); // false
