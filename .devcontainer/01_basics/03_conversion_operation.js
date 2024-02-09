ab4fbdd7bf3b8ef05becc48203cd024a88f35779
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-09 20:49:42 +0000

    operations

--- .devcontainer/01_basics/03_conversion_operation.js
+++ .devcontainer/01_basics/03_conversion_operation.js
@@ -26,5 +26,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let someString = String(someNumber)
 
-console.log(someString);
-console.log(typeof someString);
+// console.log(someString);
+// console.log(typeof someString);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+//console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "vipul"
+let str2 = " singh"
+let str3 = str1+ str2
+// console.log(str3);
+
+// console.log("1" + 2 + 2); // jb string pehle ho to string me covert ho jayenge sb
+// console.log(1+2+"2");  // jb string last me ho to pehle sare operations ho jayenge fir string me convert honge
+
+// console.log(+true); // o/p = 1
+// console.log(+""); // o/p = 0
+
+
+
+
+
+
+
