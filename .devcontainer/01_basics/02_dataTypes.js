2c3d7154ec7a08dba7701e2122f26d023c8f903b
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-09 19:53:50 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all the js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+
+let name = "vipul"
+let age = 23
+let isLoggedIn = true;
+
+
+// number=> 2 to the power 53
+// bigInt
+// string => ""
+// boolean=> true/false
+// null=> standalone value
+// undefined
+// symbol => used for uniqueness
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
