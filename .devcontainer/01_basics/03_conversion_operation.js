4f00e8ac8f10e0aad97cd0f96a68a49e3e6bc912
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-09 20:25:25 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversion_operation.js
@@ -0,0 +1,30 @@
+let score  = undefined
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true=> 1; false=>0
+// null=> number // and value is 0
+//undefined=> number// but value is NaN
+
+
+let isLoggedIn = -5
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vipul" => true
+
+let someNumber = 33
+let someString = String(someNumber)
+
+console.log(someString);
+console.log(typeof someString);
