8786ac225500d8bad1ac78095b2d97ff418c6507
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-11 21:06:32 +0000

    datatype summury

---
+++ .devcontainer/01_basics/05_dataType_summury.js
@@ -0,0 +1,19 @@
+// primitive
+
+// 7 types:  string, number, boolean, null, undefined, symbol,BigInt
+// datatype of null is object
+
+// non primitive(reference)
+// Array, objects, functions // datatype of these are object and that of functions is object function
+
+
+const hero = ["vipul","atul"];
+
+let myObj = {
+    name: "vipul"
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("hello");
+}
