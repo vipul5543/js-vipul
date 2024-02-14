2268c5645d7a97b43b1b34bfee1fa108dc0b547b
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-14 16:50:17 +0000

    maths

---
+++ .devcontainer/01_basics/07_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+
+// console.log(score);
+
+const  balance = new Number(100)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // decimal me kitne digits tk dekhna hai
+
+const otherNumber = 123.89575
+// console.log(otherNumber.toPrecision(4)); 
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ******************************** Maths ********************************
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(3.2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
