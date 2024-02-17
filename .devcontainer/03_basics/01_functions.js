fd94bd06048252bc1fb8b17911f76ccd28aef263
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2024-02-17 16:11:03 +0000

    functions

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -31,3 +31,33 @@ function loginUserMessage(username = "sam"){
 
 console.log(loginUserMessage("vipul"))
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(100,200,300,400)) // val1 = 100 , val2 = 200 and remaining wil go in num1 and more than one is going in num1 because of ...(rest operator)
+
+
+const user ={
+    name: "vipul",
+    price: 6000
+}
+
+function handleObject(anyUser){
+    console.log(`username is ${anyUser.name} and price is ${anyUser.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    name: "sam",
+    price: 1
+})
+
+const myArray = [1,2,3,4,5,6]
+
+function handleArray(getArray){
+  return getArray[1]
+}
+
+console.log(handleArray(myArray))
